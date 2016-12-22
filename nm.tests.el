c8ba7add50b18f6325b3dfe07e87348935ae807a
jherrlin <jherrlin@gmail.com>
2016-12-22 01:07:24 +0100

    More functions added.
    Unit tests based on ERT added.

---
+++ nm.tests.el
@@ -0,0 +1,19 @@
+(load-file "nm.el")
+
+(ert-deftest ert-sanity-check ()
+  "Sanity check!"
+  (should (= (+ 1 1) 2)))
+
+
+(ert-deftest ert-test-remove-trailing-spaces ()
+  "Test the remove-trailing-spaces function."
+  (should (equal (nm/remove-trailing-spaces '("a")) '("a")))
+  (should (equal (nm/remove-trailing-spaces '("a" "b")) '("a" "b")))
+  (should (equal (nm/remove-trailing-spaces '("a   " "b    ")) '("a" "b")))
+  (should (equal (nm/remove-trailing-spaces '("a   " "b    ")) '("a" "b")))
+  (should (equal (nm/remove-trailing-spaces '("Nu ska vi se   " "b    ")) '("Nu ska vi se" "b"))))
+
+
+(ert-deftest ert-test-split-and-fix-nm-output ()
+  "Test nm output"
+  (should (equal (nm/split-and-fix-nm-output "a\nb") '("b"))))
